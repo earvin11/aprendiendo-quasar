2a686efcc4de1fa1ddeee6b706992c8eb7ba8711
Earvin Gomez <earvinsantiago.11@gmail.com>
2022-11-25 16:24:35 -0400

    Primer commit

---
+++ src/pages/Flex.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-page>
+
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col col-5 bg-secondary"></div>
+      <div class="col col-6 bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-md justify-between">
+      <div class="col-12 col-sm-6 col-md-4 bg-secondary"></div>
+      <div class="col-12 col-sm-6 col-md-4 bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+    
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Flex'
+})
+</script>
+
+<style scoped>
+
+.col, .col-4, .col-6, .col-12 {
+  height: 50px;
+}
+
+.square-box {
+  height: 50px;
+  width: 50px;
+}
+
+</style>
---
+++ src/pages/Typography.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="flex column q-pa-md">
+
+    <span class="text-h1">Headline 1</span>
+    <span class="text-h2">Headline 2</span>
+    <span class="text-h3">Headline 3</span>
+    <span class="text-h4">Headline 4</span>
+    <span class="text-h5">Headline 5</span>
+    <span class="text-h6">Headline 6</span>
+
+    <q-separator />
+
+    <span class="text-subtitle1">Subtitle 1</span>
+    <span class="text-subtitle2">Subtitle 2</span>
+    
+    <q-separator />
+    <p class="text-body1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, laudantium? Doloribus velit excepturi soluta cum adipisci, itaque explicabo aspernatur accusantium.</p>
+    <p class="text-body2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, laudantium? Doloribus velit excepturi soluta cum adipisci, itaque explicabo aspernatur accusantium.</p>
+
+    <q-separator />
+    <span class="text-weight-thin">Lorem ipsum dolor sit amet consectetur.</span> 
+    <span class="text-weight-light">Lorem ipsum dolor sit amet consectetur.</span> 
+    <span class="text-weight-regular">Lorem ipsum dolor sit amet consectetur.</span> 
+    <span class="text-weight-medium">Lorem ipsum dolor sit amet consectetur.</span> 
+    <span class="text-weight-bold">Lorem ipsum dolor sit amet consectetur.</span> 
+    <span class="text-weight-bolder">Lorem ipsum dolor sit amet consectetur.</span> 
+
+
+    <q-btn
+      color="primary"
+      :label=" sideMenuOpen ? 'Cerrar Menu' : 'Abrir menu'"
+      class="q-mt-md"
+      @click="toggleSideMenu"
+     />
+
+  </q-page>
+</template>
+
+<script>
+import useUI from 'src/composables/useUI'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Typography',
+
+  setup() {
+
+    const { sideMenuOpen, toggleSideMenu } = useUI()
+
+    return {
+      sideMenuOpen,
+      toggleSideMenu
+    }
+
+  }
+})
+</script>
